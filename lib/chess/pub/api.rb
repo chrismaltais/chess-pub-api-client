c912095366ead3a396b099a5a566271c8b1eae00
Chris Maltais <chris.maltais@shopify.com>
2020-11-29 15:03:50 -0500

    Initial commit

---
+++ lib/chess/pub/api.rb
@@ -0,0 +1,10 @@
+require "chess/pub/api/version"
+
+module Chess
+  module Pub
+    module Api
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/chess/pub/api/version.rb
@@ -0,0 +1,7 @@
+module Chess
+  module Pub
+    module Api
+      VERSION = "0.1.0"
+    end
+  end
+end
